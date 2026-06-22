8689ecfcfe3c6e90b15d2c2bc6b54fd0ccce7317
Kurt Vincent Almendral Almodovar <kurtvincentalmodovar65@gmail.com>
2026-06-23 05:36:36 +0800

    refactor: split event adapters into separate classes

---
+++ src/main/java/com/shivajivarma/brs/view/adapters/ChangeAdapter.java
@@ -0,0 +1,14 @@
+package com.shivajivarma.brs.view.adapters;
+
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+public class ChangeAdapter implements ChangeListener {
+
+	@Override
+	public void stateChanged(ChangeEvent e) {
+		
+
+	}
+
+}
