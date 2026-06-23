80ab0b4dffd4e35277afb324bc7c533ab16886db
Kurt Vincent Almendral Almodovar <kurtvincentalmodovar65@gmail.com>
2026-06-24 05:18:02 +0800

    chore: renamed database columns to conventional names

--- db/data.sql
+++ db/data.sql
@@ -17,23 +17,23 @@ INSERT INTO `brs`.`route` (origin,destination) VALUES ('Ayala MRT','Binan');
 INSERT INTO `brs`.`route` (origin,destination) VALUES ('Ayala MRT','Pacita Complex');
 
 
-INSERT INTO `brs`.`bus` (routeid,ac,fare,departuretime,arrivaltime) VALUES (10,1,96,'06:00','09:00');
-INSERT INTO `brs`.`bus` (routeid,ac,fare,departuretime,arrivaltime) VALUES (10,0,76,'06:00','09:00');
-INSERT INTO `brs`.`bus` (routeid,ac,fare,departuretime,arrivaltime) VALUES (10,1,96,'10:00','13:00');
-INSERT INTO `brs`.`bus` (routeid,ac,fare,departuretime,arrivaltime) VALUES (10,0,76,'10:00','13:00');
-INSERT INTO `brs`.`bus` (routeid,ac,fare,departuretime,arrivaltime) VALUES (10,1,96,'15:00','18:00');
-INSERT INTO `brs`.`bus` (routeid,ac,fare,departuretime,arrivaltime) VALUES (10,0,76,'15:00','18:00');
+INSERT INTO `brs`.`bus` (route_id,ac,fare,departure_time,arrival_time) VALUES (10,1,96,'06:00','09:00');
+INSERT INTO `brs`.`bus` (route_id,ac,fare,departure_time,arrival_time) VALUES (10,0,76,'06:00','09:00');
+INSERT INTO `brs`.`bus` (route_id,ac,fare,departure_time,arrival_time) VALUES (10,1,96,'10:00','13:00');
+INSERT INTO `brs`.`bus` (route_id,ac,fare,departure_time,arrival_time) VALUES (10,0,76,'10:00','13:00');
+INSERT INTO `brs`.`bus` (route_id,ac,fare,departure_time,arrival_time) VALUES (10,1,96,'15:00','18:00');
+INSERT INTO `brs`.`bus` (route_id,ac,fare,departure_time,arrival_time) VALUES (10,0,76,'15:00','18:00');
 
-INSERT INTO `brs`.`bus` (routeid,ac,fare,departuretime,arrivaltime) VALUES (11,1,78,'06:00','09:00');
-INSERT INTO `brs`.`bus` (routeid,ac,fare,departuretime,arrivaltime) VALUES (11,0,58,'06:00','09:00');
-INSERT INTO `brs`.`bus` (routeid,ac,fare,departuretime,arrivaltime) VALUES (11,1,78,'10:00','13:00');
-INSERT INTO `brs`.`bus` (routeid,ac,fare,departuretime,arrivaltime) VALUES (11,0,58,'10:00','13:00');
-INSERT INTO `brs`.`bus` (routeid,ac,fare,departuretime,arrivaltime) VALUES (11,1,78,'15:00','18:00');
-INSERT INTO `brs`.`bus` (routeid,ac,fare,departuretime,arrivaltime) VALUES (11,0,58,'15:00','18:00');
+INSERT INTO `brs`.`bus` (route_id,ac,fare,departure_time,arrival_time) VALUES (11,1,78,'06:00','09:00');
+INSERT INTO `brs`.`bus` (route_id,ac,fare,departure_time,arrival_time) VALUES (11,0,58,'06:00','09:00');
+INSERT INTO `brs`.`bus` (route_id,ac,fare,departure_time,arrival_time) VALUES (11,1,78,'10:00','13:00');
+INSERT INTO `brs`.`bus` (route_id,ac,fare,departure_time,arrival_time) VALUES (11,0,58,'10:00','13:00');
+INSERT INTO `brs`.`bus` (route_id,ac,fare,departure_time,arrival_time) VALUES (11,1,78,'15:00','18:00');
+INSERT INTO `brs`.`bus` (route_id,ac,fare,departure_time,arrival_time) VALUES (11,0,58,'15:00','18:00');
 
-INSERT INTO `brs`.`bus` (routeid,ac,fare,departuretime,arrivaltime) VALUES (12,1,74,'06:00','09:00');
-INSERT INTO `brs`.`bus` (routeid,ac,fare,departuretime,arrivaltime) VALUES (12,0,64,'06:00','09:00');
-INSERT INTO `brs`.`bus` (routeid,ac,fare,departuretime,arrivaltime) VALUES (12,1,74,'10:00','13:00');
-INSERT INTO `brs`.`bus` (routeid,ac,fare,departuretime,arrivaltime) VALUES (12,0,54,'10:00','13:00');
-INSERT INTO `brs`.`bus` (routeid,ac,fare,departuretime,arrivaltime) VALUES (12,1,74,'15:00','18:00');
-INSERT INTO `brs`.`bus` (routeid,ac,fare,departuretime,arrivaltime) VALUES (12,0,54,'15:00','18:00');
+INSERT INTO `brs`.`bus` (route_id,ac,fare,departure_time,arrival_time) VALUES (12,1,74,'06:00','09:00');
+INSERT INTO `brs`.`bus` (route_id,ac,fare,departure_time,arrival_time) VALUES (12,0,64,'06:00','09:00');
+INSERT INTO `brs`.`bus` (route_id,ac,fare,departure_time,arrival_time) VALUES (12,1,74,'10:00','13:00');
+INSERT INTO `brs`.`bus` (route_id,ac,fare,departure_time,arrival_time) VALUES (12,0,54,'10:00','13:00');
+INSERT INTO `brs`.`bus` (route_id,ac,fare,departure_time,arrival_time) VALUES (12,1,74,'15:00','18:00');
+INSERT INTO `brs`.`bus` (route_id,ac,fare,departure_time,arrival_time) VALUES (12,0,54,'15:00','18:00');
--- db/setup.sql
+++ db/setup.sql
@@ -13,13 +13,13 @@ CREATE UNIQUE INDEX ROUTE_UNIQUE ON `brs`.`route`(`origin`, `destination`);
 
 CREATE TABLE  `brs`.`bus` (	
 	`id` INT NOT NULL AUTO_INCREMENT,
-	`routeid` INT NOT NULL,
+	`route_id` INT NOT NULL,
 	`ac` boolean NOT NULL, 
 	`fare` int NOT NULL, 
-	`departuretime` VARCHAR(6)  NOT NULL, 
-	`ARRIVALTIME` VARCHAR(6) NOT NULL, 
+	`departure_time` VARCHAR(6)  NOT NULL,
+	`arrival_time` VARCHAR(6) NOT NULL,
 	 PRIMARY KEY (`id`), 
-	 FOREIGN KEY (`routeid`) REFERENCES  `brs`.`route` (`id`))
+	 FOREIGN KEY (`route_id`) REFERENCES  `brs`.`route` (`id`))
      AUTO_INCREMENT=200;
      
 CREATE TABLE  `brs`.`ticket_master` (	
@@ -35,30 +35,30 @@ CREATE TABLE  `brs`.`ticket_master` (
 CREATE TABLE  `brs`.`reserve` (	
 	`id` INT NOT NULL AUTO_INCREMENT, 
 	-- `passengerid` int NOT NULL, 
-	`busid` int NOT NULL, 
+	`bus_id` int NOT NULL,
 	`dt` DATE NOT NULL, 
 	`tstamp` DATE NOT NULL, 
 	`seat` int NOT NULL, 
     `discounted` boolean NOT NULL,
 	 PRIMARY KEY (`id`), 
 	--  FOREIGN KEY (`passengerid`) REFERENCES  `brs`.`passenger` (`id`), 
-	 FOREIGN KEY (`busid`) REFERENCES  `brs`.`bus` (`id`))
+	 FOREIGN KEY (`bus_id`) REFERENCES  `brs`.`bus` (`id`))
 	 AUTO_INCREMENT = 1000;
      
-CREATE UNIQUE INDEX SEAT_UNIQUE ON `brs`.`reserve`(`busid`, `dt`, `seat`);
+CREATE UNIQUE INDEX SEAT_UNIQUE ON `brs`.`reserve`(`bus_id`, `dt`, `seat`);
 
 CREATE VIEW `brs`.`reservation` AS
     SELECT 
         `reserve`.`id` AS `id`,
         -- `reserve`.`passengerid` AS `passengerid`,
-        `reserve`.`busid` AS `busid`,
+        `reserve`.`bus_id` AS `bus_id`,
         `reserve`.`seat` AS `seat`,
         `reserve`.`dt` AS `dt`,
-        `reserve`.`tstamp` AS `tstamp`,
+        `reserve`.`timestamp` AS `timestamp`,
         `route`.`origin` AS `origin`,
         `route`.`destination` AS `destination`,
-        `bus`.`departuretime` AS `departuretime`,
-        `bus`.`ARRIVALTIME` AS `arrivaltime`,
+        `bus`.`departure_time` AS `departure_time`,
+        `bus`.`arrival_time` AS `arrival_time`,
         `bus`.`fare` AS `fare`, 
         `reserve`.`discounted` AS `discounted`
     FROM
@@ -66,6 +66,6 @@ CREATE VIEW `brs`.`reservation` AS
         JOIN `brs`.`bus`)
         JOIN `brs`.`route`)
     WHERE
-        ((`reserve`.`busid` = `bus`.`id`)
-            AND (`route`.`id` = `bus`.`routeid`))
+        ((`reserve`.`bus_id` = `bus`.`id`)
+            AND (`route`.`id` = `bus`.`route_id`))
     ORDER BY `reserve`.`id`;
